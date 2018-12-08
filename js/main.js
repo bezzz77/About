8b6dcb22ee638d54885a96c771e099883cb41002
bezzz77 <bezzz77@gmail.com>
2018-12-08 23:18:16 +0200

    edit commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
-    items:3
+    items:3,
+    autoPlay: 3000,
+    itemsDesktop: [1199, 3],
+    itemsMobile: [479, 1],
   });
 });
